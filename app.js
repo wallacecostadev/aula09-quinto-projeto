6ad75db3372a7c2782d17c4ecbfd2a354f7fbff5
Wallace Costa <50935321+wallacecostadev@users.noreply.github.com>
2026-01-09 20:23:35 -0300

    Add DOMContentLoaded event listener for styling

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  var corpo = document.body
+  corpo.style.background = 'rgb(40, 200, 200)'
+
+  var p1 = document.getElementsByTagName('p')[1]
+  p1.style.color = 'blue'
+
+  var d = document.querySelector('div#msg')
+  d.style.background = 'green'
+
+  // NÃO use document.write
+  console.log(p1.innerHTML)
+
+})
 
